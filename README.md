4939e122259dc07cf4cc80cb4740aaffab9392f4
Manuel Briand <manuel.briand354@gmail.com>
2017-10-18 13:42:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+flyaround
+=========
+
+A Symfony project created on October 15, 2017, 1:49 pm.
---
+++ src/WCS/CoavBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WCS\CoavBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('WCSCoavBundle:Default:index.html.twig');
+    }
+}
